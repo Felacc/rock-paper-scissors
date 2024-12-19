738cd5ae1b256eb4a56f93803a0f904ae4003633
Felacc <98856469+Felacc@users.noreply.github.com>
2024-12-18 16:19:22 -0800

    Create project files and write pseudocode

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Testing");
+
+
+// Pseudocode
+// Create variable for human score
+// Create variable for computer score
+// Create a variable for human choice
+// Create a variable for computer choice
+// Write function for getting computer choice
+//     generate random number 1-3 (inclusive)
+//     IF number is 1, return "rock"
+//     ELIF number is 2, return "paper"
+//     ELSE number is 3, return "scissors"
+// Write a function for getting human input
+//     prompt user for input, and store the answer in a variable
+//     check that user input is either "rock", "paper", or "scissors"
+//         standardize input by making it lowercase and trimming whitespace
+//         compare strings against desired inputs
+//         IF the input is valid (rock, paper, or scissors), return it
+//         ELSE ask the user to try again
+// Declare the player and computer scores
+// Write a function for playing a round that uses human choice and computer choice as parameters
+//     IF computer chose paper and human chose rock, display message that human wins the round, and increment human's score by 1
+//     etc...
+// FOR 5 rounds, record scores, and declare a winner at the end
