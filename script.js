b3488731e827d94b15372c47b381dfafd11a1f8c
Felacc <98856469+Felacc@users.noreply.github.com>
2025-01-14 21:00:31 -0800

    Add feature that lets buttons play rounds

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-// Pseudocode
-// Create variable for human score
-// Create variable for computer score
-// Create a variable for human choice
-// Create a variable for computer choice
-// Write function for getting computer choice
-//     generate random number 1-3 (inclusive)
-//     IF number is 1, return "rock"
-//     ELIF number is 2, return "paper"
-//     ELSE number is 3, return "scissors"
-// Write a function for getting human input
-//     prompt user for input, and store the answer in a variable
-//     check that user input is either "rock", "paper", or "scissors"
-//         standardize input by making it lowercase and trimming whitespace
-//         compare strings against desired inputs
-//         IF the input is valid (rock, paper, or scissors), return it
-//         ELSE ask the user to try again
-// Declare the player and computer scores
-// Write a function for playing a round that uses human choice and computer choice as parameters
-//     IF computer chose paper and human chose rock, display message that human wins the round, and increment human's score by 1
-//     etc...
-// FOR 5 rounds, record scores, and declare a winner at the end
-
-
-// Global variables:
-// Scores declared, initialized and set to 0
-// Choice variables declared, but not set to anything yet
 let computerScore = 0;
 let humanScore = 0;
 let computerChoice;
 let humanChoice;
 
+
+// 
+const r_btn = document.querySelector("#r-btn");
+const p_btn = document.querySelector("#p-btn");
+const s_btn = document.querySelector("#s-btn");
+
+r_btn.addEventListener("click", () => {
+    playRound(getComputerChoice(), "rock");
+});
+
+p_btn.addEventListener("click", () => {
+    playRound(getComputerChoice(), "paper");
+});
+
+s_btn.addEventListener("click", () => {
+    playRound(getComputerChoice(), "scissors");
+});
 // Start game of RPS
 playGame();
 
@@ -52,23 +42,23 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    // get initial value from user
-    let humanChoice = prompt("Your move.", "");
-    while (true) {
-        // format string
-        humanChoice = humanChoice.trim();
-        humanChoice = humanChoice.toLowerCase();
+// function getHumanChoice() {
+//     // get initial value from user
+//     let humanChoice = prompt("Your move.", "");
+//     while (true) {
+//         // format string
+//         humanChoice = humanChoice.trim();
+//         humanChoice = humanChoice.toLowerCase();
 
-        // check if inputted value is a not valid move, prompt fo`r a new input, then loop
-        // if input is valid return value
-        if (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
-            humanChoice = prompt("Invalid move.", "");
-        } else {
-            return humanChoice;
-        }
-    }
-}
+//         // check if inputted value is a not valid move, prompt fo`r a new input, then loop
+//         // if input is valid return value
+//         if (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
+//             humanChoice = prompt("Invalid move.", "");
+//         } else {
+//             return humanChoice;
+//         }
+//     }
+// }
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === humanChoice) {
