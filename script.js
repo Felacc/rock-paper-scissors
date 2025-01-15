5deb7cf699056f67dcd57ce1215f635024a66e8d
Felacc <98856469+Felacc@users.noreply.github.com>
2025-01-14 21:41:38 -0800

    Refactor so that game is played with the buttons
    Change playGame() to displayResults() and use the function to manipulate
    the DOM based on the state of the game.

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ let computerChoice;
 let humanChoice;
 
 
-// 
+// Button event listeners
 const r_btn = document.querySelector("#r-btn");
 const p_btn = document.querySelector("#p-btn");
 const s_btn = document.querySelector("#s-btn");
 
 r_btn.addEventListener("click", () => {
     playRound(getComputerChoice(), "rock");
+    displayResults();
 });
 
 p_btn.addEventListener("click", () => {
     playRound(getComputerChoice(), "paper");
+    displayResults();
 });
 
 s_btn.addEventListener("click", () => {
     playRound(getComputerChoice(), "scissors");
+    displayResults();
 });
 
 
@@ -83,22 +86,30 @@ function playRound(computerChoice, humanChoice) {
     }
 }
 
-function playGame() {
+function displayResults() {
     // Display current scores
-    console.log("Initial Scores\n" + "Player Score: " + humanScore + "\nComputer Score: " + computerScore);
+    const resultsDiv = document.createElement("div");
+    const body = document.querySelector("body");
+    const buttons = document.querySelector(".buttons");
 
-    console.log("Final Scores\n" + "Player Score: " + humanScore + "\nComputer Score: " + computerScore);
+    resultsDiv.textContent = "Your Score: " + humanScore + " Computer Score: " + computerScore;
+    
+    // if score of 5 is reached remove buttons and display result
+    if (humanScore == 5) {
+        buttons.removeChild(r_btn);
+        buttons.removeChild(p_btn);
+        buttons.removeChild(s_btn);
 
-    if (humanScore == computerScore) {
-        console.log("The game is a tie!")
+        resultsDiv.textContent = "You won the game!";
     }
 
-    if (humanScore > computerScore) {
-        console.log("You won the game!")
+    if (computerScore == 5) {
+        buttons.removeChild(r_btn);
+        buttons.removeChild(p_btn);
+        buttons.removeChild(s_btn);
+        resultsDiv.textContent = "You lost the game.";
     }
 
-    if (humanScore < computerScore) {
-        console.log("You lost the game.")
-    }
+    body.appendChild(resultsDiv);
 
 }
